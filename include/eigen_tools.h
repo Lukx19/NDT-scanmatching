cd27edf1e6f3a664dff9389aab7d9191d23c2490
Lukas Jelinek <lukas.jelinek1@gmail.com>
2016-03-18 18:44:23 +0100

    Fixing error in Eigen tools lib

--- include/eigen_tools.h
+++ include/eigen_tools.h
@@ -4,6 +4,10 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643383279502884197169399375105820974944592
+#endif
+
 namespace eigt
 {
 template <typename T>
@@ -15,32 +19,48 @@ using point2d_t = Eigen::Matrix<T, 2, 1>;
 template <typename T>
 using pose2d_t = Eigen::Matrix<T, 3, 1>;
 // ************************* DECLARATION *************************************
+
+/// Calculates transformation between inputed poses. 
 template <typename T>
 transform2d_t<T> transBtwPoses(const pose2d_t<T> &from, const pose2d_t<T> &to);
 
+// Transforms pose in global frame to its new posistion based on transform
 template <typename T>
 pose2d_t<T> transformPose(const pose2d_t<T> &pose,
                           const transform2d_t<T> &trans);
 
+// maps from transformation matrix 3x3 to vector 
+// [delta_x,delta_y, detla_angle]
 template <typename T>
 pose2d_t<T> getPoseFromTransform(const transform2d_t<T> &trans);
 
+// returns angle which is rotational part of transformation
 template <typename T>
 T getAngle(const transform2d_t<T> &trans);
 
+// returns square distance from translation part of transform
 template <typename T>
 T getDisplacement(const transform2d_t<T> &trans);
 
+// returns absolute angular diffrence between poses. Selects shorter angle.
 template <typename T>
 T getAngleDiffrence(const pose2d_t<T> &from, const pose2d_t<T> &to);
 
+// Maps 4x4 transformation matrix to 3x3 transformation matrix
 template <typename T>
-transform2d_t<T>
-convertTransform(const Eigen::Matrix<T, 4, 4> &trans);
+transform2d_t<T> convertTransform(const Eigen::Matrix<T, 4, 4> &trans);
+
+// MAPS from 3x3 transformation matrix to 4x4 Transformation matrix
+template <typename T>
+Eigen::Matrix<T, 4, 4> convertTransform(const transform2d_t<T> &trans);
+
+// Maps transformation encoded in pose vector [delta_x,delta_y,delta_angle] to
+// transformation matrix
+template <typename T>
+eigt::transform2d_t<T> getTransFromPose(const pose2d_t<T> &trans);
 
 template <typename T>
-Eigen::Matrix<T, 4, 4> convertTransform(
-    const transform2d_t<T> &trans);
+T normalizeAngle(T angle);
 }
 // ************************* IMPLEMENTATION****************************
 template <typename T>
@@ -48,11 +68,13 @@ eigt::transform2d_t<T> eigt::transBtwPoses(const pose2d_t<T> &from,
                                            const pose2d_t<T> &to)
 {
   transform2d_t<T> t;
+  Eigen::Rotation2D<T> rot_from(from(2));
+  Eigen::Rotation2D<T> rot_to(to(2));
   t.setIdentity();
-  T angle = getAngleDiffrence(from, to);
-  Eigen::Rotation2D<T> rot(angle);
-  t.matrix().block(0,0,2,2) = rot.toRotationMatrix();
-  t.matrix().block(0,2,2,1) = to.head(2) - rot.toRotationMatrix() * from.head(2);
+  t.matrix().block(0, 0, 2, 2) =
+      (rot_from.toRotationMatrix().transpose() * rot_to.toRotationMatrix());
+  t.matrix().block(0, 2, 2, 1) =
+      rot_from.toRotationMatrix() * (to.head(2) - from.head(2));
   return t;
 }
 
@@ -60,18 +82,22 @@ template <typename T>
 eigt::pose2d_t<T> eigt::transformPose(const pose2d_t<T> &pose,
                                       const transform2d_t<T> &trans)
 {
-  transform2d_t<T> t;
-  t = trans *
-      t.fromPositionOrientationScale(
-          pose.head(2), Eigen::Rotation2D<T>(pose(2)), point2d_t<T>::Ones());
-  return getPoseFromTransform<T>(t);
+  Eigen::Rotation2D<T> rot(pose(2));
+  pose2d_t<T> res_pose;
+  pose2d_t<T> inc = getPoseFromTransform(trans);
+  res_pose.head(2) = pose.head(2) + rot.toRotationMatrix().transpose() * inc.head(2);
+  res_pose(2) = normalizeAngle(pose(2) + inc(2));
+  return res_pose;
 }
 
 template <typename T>
 eigt::pose2d_t<T> eigt::getPoseFromTransform(const transform2d_t<T> &trans)
 {
   pose2d_t<T> pose;
-  pose << trans(0, 2), trans(1, 2), std::atan2(trans(1, 0), trans(0, 0));
+  auto translation = trans.translation();
+  pose(0) = translation(0);
+  pose(1) = translation(1);
+  pose(2) = eigt::getAngle(trans);
   return pose;
 }
 
@@ -95,11 +121,13 @@ T eigt::getAngleDiffrence(const pose2d_t<T> &from, const pose2d_t<T> &to)
 }
 
 template <typename T>
-eigt::transform2d_t<T> eigt::convertTransform(const Eigen::Matrix<T, 4, 4> &trans)
+eigt::transform2d_t<T>
+eigt::convertTransform(const Eigen::Matrix<T, 4, 4> &trans)
 {
   transform2d_t<T> new_trans;
-  new_trans.matrix().block(0,0,2,2) = trans.block(0,0,2,2);
-  new_trans.matrix().block(0,2,2,0) = trans.block(0,3,2,1);
+  new_trans.setIdentity();
+  new_trans.matrix().block(0, 0, 2, 2) = trans.block(0, 0, 2, 2);
+  new_trans.matrix().block(0, 2, 2, 1) = trans.block(0, 3, 2, 1);
   return new_trans;
 }
 
@@ -108,9 +136,25 @@ Eigen::Matrix<T, 4, 4> eigt::convertTransform(const transform2d_t<T> &trans)
 {
   Eigen::Matrix<T, 4, 4> new_trans;
   pose2d_t<T> pose = getPoseFromTransform<T>(trans);
-  Eigen::AngleAxis<T> init_rotation(pose(2), Eigen::Matrix<T,3,1>::UnitZ());
+  Eigen::AngleAxis<T> init_rotation(pose(2), Eigen::Matrix<T, 3, 1>::UnitZ());
   Eigen::Translation<T, 3> init_translation(pose(0), pose(1), 0);
   return (init_translation * init_rotation).matrix();
+}
+
+template <typename T>
+eigt::transform2d_t<T> eigt::getTransFromPose(const pose2d_t<T> &trans)
+{
+  transform2d_t<T> trans_mtx;
+  Eigen::Rotation2D<T> rot(trans(2));
+  Eigen::Translation<T, 2> transl(trans.head(2));
+  return transl * rot;
+}
+
+template <typename T>
+T eigt::normalizeAngle(T angle)
+{
+  return atan2(sin(angle), cos(angle));
+  //return angle - 2 * M_PI *std::floor((angle + M_PI) / (2 * M_PI));
 }
 
 #endif
--- test/eigen_tools_test.cpp
+++ test/eigen_tools_test.cpp
@@ -15,10 +15,10 @@ TEST(EigenTools, createTransFromPosesSimple)
     pose2d_t<double> second_pose;
     second_pose << 10, 10, 0;
     transform2d_t<double> res_trans;
-    res_trans.matrix() << 1,0,10,0,1,10,0,0,1;
-    auto calc = transBtwPoses(first_pose,second_pose).matrix();
-    EXPECT_TRUE ((res_trans.matrix() - calc).cwiseAbs().sum() < EPSILON);
-    //ADD_FAILURE() <<res_trans.matrix()<< "\n\n"<< calc;
+    res_trans.matrix() << 1, 0, 10, 0, 1, 10, 0, 0, 1;
+    auto calc = transBtwPoses(first_pose, second_pose).matrix();
+    EXPECT_TRUE((res_trans.matrix() - calc).cwiseAbs().sum() < EPSILON);
+    // ADD_FAILURE() <<res_trans.matrix()<< "\n\n"<< calc;
   } catch (...) {
     ADD_FAILURE() << "Uncaught exception";
   }
@@ -32,10 +32,152 @@ TEST(EigenTools, createTransFromPosesAdvance)
     pose2d_t<double> second_pose;
     second_pose << 20, 20, 0;
     transform2d_t<double> res_trans;
-    res_trans.matrix() << 1,0,10,0,1,10,0,0,1;
-    auto calc = transBtwPoses(first_pose,second_pose).matrix();
-    EXPECT_TRUE ((res_trans.matrix() - calc).cwiseAbs().sum() < EPSILON);
-    //ADD_FAILURE() <<res_trans.matrix()<< "\n\n"<< calc;
+    res_trans.matrix() << 1, 0, 10, 0, 1, 10, 0, 0, 1;
+    auto calc = transBtwPoses(first_pose, second_pose).matrix();
+    EXPECT_TRUE((res_trans.matrix() - calc).cwiseAbs().sum() < EPSILON);
+    // ADD_FAILURE() <<res_trans.matrix()<< "\n\n"<< calc;
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, createTransFromPosesAngle)
+{
+  try {
+    pose2d_t<double> first_pose;
+    first_pose << 0, 0, 0;
+    pose2d_t<double> second_pose;
+    second_pose << 0, 0, M_PI;
+    transform2d_t<double> res_trans;
+    res_trans.matrix() << -1, 0, 0, 0, -1, 0, 0, 0, 1;
+    auto calc = transBtwPoses(first_pose, second_pose).matrix();
+    EXPECT_TRUE((res_trans.matrix() - calc).cwiseAbs().sum() < EPSILON);
+    // ADD_FAILURE() <<res_trans.matrix()<< "\n\n"<< calc;
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, CycleTest)
+{
+  try {
+    pose2d_t<double> first_pose;
+    first_pose << 10, 20, 0;
+    pose2d_t<double> second_pose;
+    second_pose << -10, 30, -M_PI;
+    auto calc_trans = transBtwPoses(first_pose, second_pose);
+    auto calc_sec_pose = transformPose(first_pose,calc_trans);
+    std::cout<<getAngle(calc_trans)<<"\n";
+    std::cout<<getPoseFromTransform(calc_trans)<<"\n";
+    std::cout<<"target pose: " <<second_pose.transpose()<<" calc target pose: " << calc_sec_pose.transpose()<< "\n";
+    EXPECT_TRUE((calc_sec_pose - second_pose).cwiseAbs().sum() < EPSILON);
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, CycleTest2)
+{
+  try {
+    pose2d_t<double> first_pose;
+    first_pose << -10, 5, 0;
+    pose2d_t<double> second_pose;
+    second_pose << 10, 6, 2;
+    auto calc_trans = transBtwPoses(first_pose, second_pose);
+    auto calc_sec_pose = transformPose(first_pose,calc_trans);
+    std::cout<<getAngle(calc_trans)<<"\n";
+    std::cout<<getPoseFromTransform(calc_trans)<<"\n";
+    std::cout<<"target pose: " <<second_pose.transpose()<<" calc target pose: " << calc_sec_pose.transpose()<< "\n";
+    EXPECT_TRUE((calc_sec_pose - second_pose).cwiseAbs().sum() < EPSILON);
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, CycleTest3)
+{
+  try {
+    pose2d_t<double> first_pose;
+    first_pose << -10, 5, 1;
+    pose2d_t<double> second_pose;
+    second_pose << 10, 6, M_PI;
+    auto calc_trans = transBtwPoses(first_pose, second_pose);
+    auto calc_sec_pose = transformPose(first_pose,calc_trans);
+    std::cout<<getAngle(calc_trans)<<"\n";
+    std::cout<<getPoseFromTransform(calc_trans)<<"\n";
+    std::cout<<"target pose: " <<second_pose.transpose()<<" calc target pose: " << calc_sec_pose.transpose()<< "\n";
+    EXPECT_TRUE((calc_sec_pose - second_pose).cwiseAbs().sum() < EPSILON);
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, complexInterfaceTest)
+{
+  try {
+    pose2d_t<double> source_pose;
+    pose2d_t<double> target_pose;
+    transform2d_t<double> trans_btw_poses;
+    pose2d_t<double> trans_btw_poses_short;
+    double angle_diff;
+    double displacement;
+
+    source_pose << -10, 5, 1.2;
+    target_pose << 10, 6, 2;
+    angle_diff = 0.8;
+    displacement = std::sqrt(std::pow(source_pose(0) - target_pose(0), 2) +
+                             std::pow(source_pose(1) - target_pose(1), 2));
+    trans_btw_poses_short << 6.31512, 19.0031, 0.8;
+    auto calc_trans = transBtwPoses(source_pose, target_pose);
+    auto calc_angle = getAngle(calc_trans);
+    auto calc_angle_diff = getAngleDiffrence(source_pose,target_pose);
+    auto calc_displacement = getDisplacement(calc_trans);
+    auto calc_pose = getPoseFromTransform(calc_trans);
+    auto calc_trans_pose = transformPose(source_pose, calc_trans);
+    std::cout<< calc_trans.matrix() << "\n";
+    std::cout<<"angle: " <<calc_angle << "\n";
+    std::cout<<"angle diffrence: " <<calc_angle_diff << "\n";
+    std::cout<<"displacement: " <<calc_displacement << "\n";
+    std::cout<<"pose from transform: " <<calc_pose << "\n";
+     std::cout<<"transformed source pose: " <<calc_trans_pose.transpose() <<
+              "target pose reference: " << target_pose.transpose()<<"\n";
+    
+    EXPECT_TRUE((trans_btw_poses_short - calc_pose)
+                    .cwiseAbs()
+                    .sum() < EPSILON);
+
+    EXPECT_TRUE(std::abs(angle_diff - calc_angle) < EPSILON);
+    EXPECT_TRUE(std::abs(displacement - calc_displacement) < EPSILON);
+    EXPECT_TRUE((target_pose - calc_trans_pose).cwiseAbs().sum() <
+        EPSILON);
+
+  } catch (...) {
+    ADD_FAILURE() << "Uncaught exception";
+  }
+}
+
+TEST(EigenTools, normalizeAngle){
+  EXPECT_LT(normalizeAngle(M_PI) - M_PI,EPSILON);
+  EXPECT_LT(normalizeAngle(3*M_PI) - M_PI,EPSILON);
+  EXPECT_LT(normalizeAngle(2*M_PI) ,EPSILON);
+  EXPECT_LT(normalizeAngle(3*(M_PI/2)) - M_PI + (M_PI/2) ,EPSILON);
+
+  EXPECT_LT(normalizeAngle(-M_PI) + M_PI,EPSILON);
+  EXPECT_LT(normalizeAngle(-2*M_PI),EPSILON);
+  EXPECT_LT(normalizeAngle(-3*M_PI) + M_PI,EPSILON);
+}
+
+
+TEST(EigenTools, getTransFromPoseTest)
+{
+  try {
+    pose2d_t<double> first_pose;
+    first_pose << -10, 5, 0;
+    pose2d_t<double> second_pose;
+    second_pose << 10, 6, 2;
+    auto calc_trans = transBtwPoses(first_pose, second_pose);
+    auto calc_pose = getPoseFromTransform(calc_trans);
+    EXPECT_LT((calc_trans.matrix() - getTransFromPose(calc_pose).matrix()).cwiseAbs().sum(),EPSILON);
   } catch (...) {
     ADD_FAILURE() << "Uncaught exception";
   }
